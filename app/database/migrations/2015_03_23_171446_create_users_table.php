d56a65a7d44ae7f84f140f7aa188ccae9022406a
Kirill Chalkin <nonamez123@gmail.com>
2015-03-23 19:17:35 +0200

    migration added

---
+++ app/database/migrations/2015_03_23_171446_create_users_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration {
+
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('email')->unique();
+			$table->string('password');
+			$table->timestamps();
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('users');
+	}
+
+}
