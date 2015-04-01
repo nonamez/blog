0fc6141cd93dc24779d69f7d9d8893f18ab58dac
NoNameZ <hello@nonamez.name>
2015-04-01 14:56:49 +0300

    User migration creatred

--- app/database/migrations/2015_03_23_171446_create_users_table.php
+++ app/database/migrations/2015_03_23_171446_create_users_table.php
@@ -7,11 +7,13 @@ class CreateUsersTable extends Migration {
 
 	public function up()
 	{
-		Schema::table('users', function(Blueprint $table)
+		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('email')->unique();
 			$table->string('password');
+			$table->boolean('is_admin')->default(FALSE);
+			$table->rememberToken();
 			$table->timestamps();
 		});
 	}
