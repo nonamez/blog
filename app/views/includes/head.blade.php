fc47ca49f391dcbe2ea9eee0573e2950c3c338af
Kirill Chalkin <nonamez123@gmail.com>
2015-04-07 22:10:41 +0300

    Misc

--- app/views/includes/head.blade.php
+++ app/views/includes/head.blade.php
@@ -1,15 +1,14 @@
 <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="HandheldFriendly" content="True">
 
 <title>@section('title') @lang('blog.title') @show</title>
 
 <meta name="csrf-token" content="<?php echo csrf_token();?>">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="HandheldFriendly" content="True">
 
 <meta name="description" content="{{ $meta_description or Lang::get('blog.meta.description') }}"> 
 <meta name="keywords" content="{{ $meta_keywords or Lang::get('blog.meta.keywords') }}"> 
-
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 <link rel="stylesheet" type="text/css" href="{{ asset('/assets/plugins/bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" type="text/css" href="{{ asset('/assets/blog/css/app.css') }}">
