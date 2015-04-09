f52cdc2fdfe2980d814f3059e25fe7b51b2990b9
NoNameZ <hello@nonamez.name>
2015-04-09 17:06:10 +0300

    Created post "pill" with dropdown

--- app/views/includes/admin/header.blade.php
+++ app/views/includes/admin/header.blade.php
@@ -1,22 +1,17 @@
 <header class="clearfix">
 	<nav>
 		<ul class="nav nav-pills pull-right">
-			<li role="presentation" class="active"><a href="index.html#">Home</a></li>
-			<li role="presentation"><a href="index.html#">About</a></li>
-			<li role="presentation"><a href="index.html#">Contact</a></li>
-			<li class="dropdown" role="presentation">
+			<li class="dropdown active" role="presentation">
 				<a aria-expanded="false" role="button" href="#" data-toggle="dropdown" class="dropdown-toggle">
-					Dropdown <span class="caret"></span>
+					Posts <span class="caret"></span>
 				</a>
 				<ul role="menu" class="dropdown-menu">
-					<li><a href="#">Action</a></li>
-					<li><a href="#">Another action</a></li>
-					<li><a href="#">Something else here</a></li>
+					<li><a href="{{ URL::action('Admin\PostController@index') }}">All</a></li>
 					<li class="divider"></li>
-					<li><a href="#">Separated link</a></li>
+					<li><a href="{{ URL::action('Admin\PostController@create') }}">Create</a></li>
 				</ul>
 			</li>
 		</ul>
 	</nav>
-	<h3 class="text-muted">Project name</h3>
+	<h3 class="text-muted">Admin Panel</h3>
 </header>
