3fd7613469c517fb1abf5ec87a8283056a7655c8
NoNameZ <hello@nonamez.name>
2015-04-27 14:40:59 +0300

    Files migration created

---
+++ app/database/migrations/2015_04_27_063644_create_blg_files_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlgFilesTable extends Migration {
+
+	public function up()
+	{
+		Schema::create('blg_files', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name');
+			$table->string('path');
+			$table->integer('post_id')->unsigned()->nullable()->index();
+			$table->timestamps();
+			
+			$table->foreign('post_id')->references('id')->on('blg_translated_posts');
+		});
+	}
+
+	public function down()
+	{
+		Schema::drop('blg_files');
+	}
+
+}
