e5a80b4eabdc2d5fd713f64904815ba8e9498a06
NoNameZ <hello@nonamez.name>
2015-05-07 11:02:48 +0300

    Some fixes

--- app/views/includes/blog/head.blade.php
+++ app/views/includes/blog/head.blade.php
@@ -10,17 +10,13 @@
 <meta name="description" content="{{ $meta_description or Lang::get('blog.meta.description') }}"> 
 <meta name="keywords" content="{{ $meta_keywords or Lang::get('blog.meta.keywords') }}"> 
 
-<link rel="stylesheet" type="text/css" href="{{ asset('/assets/plugins/bootstrap/css/bootstrap.min.css') }}">
-<link rel="stylesheet" type="text/css" href="{{ asset('/assets/blog/app.css') }}">
+<link rel="stylesheet" href="{{ asset('/assets/plugins/bootstrap/css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/assets/plugins/font-awesome/css/font-awesome.min.css') }}">
 
-<link href="{{ asset('/assets/plugins/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('/assets/blog/app.css') }}">
 
 @yield('custom_styles')
-<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-<![endif]-->
+
 <script>var root_url = '{{ URL::to('/') }}'</script>
 
 <link rel="canonical" href="{{ URL::current() }}">
