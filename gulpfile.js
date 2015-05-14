ada352bb6bd382f145a1c8ab57ce69b7616d6f42
Kirill Chalkin <nonamez123@gmail.com>
2015-05-14 18:51:00 +0300

    "Gulp" tasks update

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
+const BOWER_DIR  = 'bower_components';
+const PLUGIN_DIR = 'public/assets/plugins';
+
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-	gulp.src(['vendor/bower_components/jquery/dist/**/*']).pipe(gulp.dest('public/assets/plugins/jquery/'));
-	gulp.src(['vendor/bower_components/bootstrap/dist/**/*']).pipe(gulp.dest('public/assets/plugins/bootstrap/'));
-	gulp.src(['vendor/bower_components/fontawesome/css']).pipe(gulp.dest('public/assets/plugins/fontawesome/css'));
-	gulp.src(['vendor/bower_components/fontawesome/fonts']).pipe(gulp.dest('public/assets/plugins/fontawesome/fonts'));
+gulp.task('copy', function() {
+	gulp.src([BOWER_DIR + '/jquery/dist/**/*']).pipe(gulp.dest(PLUGIN_DIR + '/jquery/'));
+	gulp.src([BOWER_DIR + '/bootstrap/dist/**/*']).pipe(gulp.dest(PLUGIN_DIR + '/bootstrap/'));
+	gulp.src(['css/**/*', 'fonts/**/*'], {'cwd': BOWER_DIR + '/fontawesome', 'base': BOWER_DIR + '/fontawesome'}).pipe(gulp.dest(PLUGIN_DIR + '/fontawesome/'));
 });
+
+gulp.task('default', ['copy']);
