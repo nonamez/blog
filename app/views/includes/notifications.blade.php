87a81965b420e568de7d5e9ba678ce7554108303
Kirill Chalkin <nonamez123@gmail.com>
2015-05-18 23:47:03 +0300

    Success notice added

--- app/views/includes/notifications.blade.php
+++ app/views/includes/notifications.blade.php
@@ -13,3 +13,9 @@
 	{{ Session::get('notice') }}
 </div>
 @endif
+@if (Session::has('success'))
+<div class="alert alert-success">
+	<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	{{ Session::get('success') }}
+</div>
+@endif
