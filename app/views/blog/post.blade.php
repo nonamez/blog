0e872c4b6d48a2c8432617b4985d6415dabb671f
NoNameZ <hello@nonamez.name>
2015-05-21 11:45:48 +0300

    Fixed social share buttons

--- app/views/blog/post.blade.php
+++ app/views/blog/post.blade.php
@@ -20,13 +20,13 @@
 		</section>
 		<footer class="post-footer">
 			<section class="share">
-				<a class="fa fa-twitter-square twitter" href="https://twitter.com/share?text={{ $post['title'] }}&amp;url={{ URL::current() }}" onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;" rel="tooltip" title="Отметить в Twitter">
+				<a class="fa fa-twitter-square twitter" href="https://twitter.com/share?text={{{ $post['title'] }}}&amp;url={{ URL::current() }}" onclick="window.open(this.href, 'twitter-share', 'width=550,height=235');return false;">
 					<span class="hidden">Twitter</span>
 				</a>
-				<a class="fa fa-facebook-square facebook" href="https://www.facebook.com/sharer/sharer.php?u={{ URL::current() }}" onclick="window.open(this.href, 'facebook-share','width=580,height=296');return false;" rel="tooltip" title="Отметить в Facebook">
+				<a class="fa fa-facebook-square facebook" href="https://www.facebook.com/sharer/sharer.php?u={{ URL::current() }}" onclick="window.open(this.href, 'facebook-share','width=580,height=296');return false;">
 					<span class="hidden">Facebook</span>
 				</a>
-				<a class="fa fa-google-plus-square google-plus" href="https://plus.google.com/share?url={{ URL::current() }}" onclick="window.open(this.href, 'google-plus-share', 'width=490,height=530');return false;" rel="tooltip" title="Отметить в Google+">
+				<a class="fa fa-google-plus-square google-plus" href="https://plus.google.com/share?url={{ URL::current() }}" onclick="window.open(this.href, 'google-plus-share', 'width=490,height=530');return false;">
 					<span class="hidden">Google+</span>
 				</a>
 			</section>
