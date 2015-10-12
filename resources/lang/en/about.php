7159a124217b6478a70ed81fde5263cfefd4ef97
NoNameZ <hello@nonamez.name>
2015-10-12 15:24:34 +0300

    Changed "About Me" page

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+	'iam' => ' Hey! I\'m Kiril - self-taught freelance web developer based in Klaipeda, Lithuania. ',
 	'skills' => [
 		'title' => 'Briefly about my skills:',
 		'li_1'  => 'I think I have a good knowledge of PHP, MySQL, JS/jQuery.',
@@ -8,6 +9,6 @@ return [
 		'li_3'  => 'Sometimes it seems that I know Python.',
 		'li_4'  => 'Have read some stuff about NodeJS.'
 	],
-	'contact' => 'You can conctact me via <img style="vertical-align:baseline" src=":img" alt="Email address">',
+	'contact' => 'You can conctact me via :img',
 	'ps' => 'I like simple things. And coding. And cats. And music. And sports.'
 ];
