65ac2f10d122d21609672fa641e4cfe8c7e3e63c
Kiril <hello@nonamez.name>
2016-01-15 11:05:40 +0200

    Added new value to fillable

--- app/Models/File.php
+++ app/Models/File.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class File extends Model {
 
 	protected $table = 'files';
-	protected $fillable = ['name', 'original_name'];
+	protected $fillable = ['name', 'original_name', 'type'];
 
 	public function post()
 	{
