250b53e9060e2dfb0f821163ea4e0ca04f788b3b
Kiril <hello@nonamez.name>
2016-02-04 14:53:22 +0200

    Syntax highlight changed

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('copy', function() {
 	gulp.src([BOWER_DIR + '/jquery/dist/**/*']).pipe(gulp.dest(PLUGIN_DIR + '/jquery/'));
 	gulp.src([BOWER_DIR + '/bootstrap/dist/**/*']).pipe(gulp.dest(PLUGIN_DIR + '/bootstrap/'));
 	gulp.src(['css/**/*', 'fonts/**/*'], {'cwd': BOWER_DIR + '/fontawesome', 'base': BOWER_DIR + '/fontawesome'}).pipe(gulp.dest(PLUGIN_DIR + '/fontawesome/'));
-	gulp.src(['highlight.pack.min.js', 'styles/vs.css'], {'cwd': BOWER_DIR + '/highlightjs', 'base': BOWER_DIR + '/highlightjs'}).pipe(gulp.dest(PLUGIN_DIR + '/highlightjs/'));
+	gulp.src(['highlight.pack.min.js', 'styles/ascetic.css'], {'cwd': BOWER_DIR + '/highlightjs', 'base': BOWER_DIR + '/highlightjs'}).pipe(gulp.dest(PLUGIN_DIR + '/highlightjs/'));
 });
 
 gulp.task('default', ['copy']);
--- resources/views/includes/blog/head.blade.php
+++ resources/views/includes/blog/head.blade.php
@@ -12,7 +12,7 @@
 
 <link rel="stylesheet" href="{{ asset('/assets/plugins/bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('/assets/plugins/fontawesome/css/font-awesome.min.css') }}">
-<link rel="stylesheet" href="{{ asset('/assets/plugins/highlightjs/styles/vs.css') }}">
+<link rel="stylesheet" href="{{ asset('/assets/plugins/highlightjs/styles/ascetic.css') }}">
 
 <link rel="stylesheet" href="{{ asset('/assets/blog/styles.css') }}">
 
