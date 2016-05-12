c8af563f1f29b4b43e804ee8bda303dbd09c1f9a
Kiril <hello@nonamez.name>
2016-05-12 15:55:50 +0300

    AjaxResponse method added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,4 +11,29 @@ use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 class Controller extends BaseController
 {
     use AuthorizesRequests, AuthorizesResources, DispatchesJobs, ValidatesRequests;
+
+    protected function ajaxResponse(array $input_data = [], $show_data_on_error = FALSE) {
+		$result = array_key_exists('error', $input_data) ? (! (bool) $input_data['error']) : TRUE;
+		
+		if (array_key_exists('data', $input_data))
+			$data = $result ? $input_data['data'] : ($show_data_on_error ? $input_data['data'] : NULL);
+		else
+			$data = NULL;
+		
+		if (array_key_exists('message', $input_data))
+			$message = $input_data['message'];
+		else
+			$message = $result ? trans('basics.ajax_response.true') : trans('basics.ajax_response.false');
+		
+		$err_type = array_key_exists('err_type', $input_data) ? $input_data['err_type'] : FALSE;
+		
+		$result = [
+			'data'     => $data,
+			'status'   => $result,
+			'message'  => $message,
+			'err_type' => $err_type
+		];
+		
+		return response()->json($result);
+	}
 }
