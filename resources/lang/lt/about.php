dc14c431727440c2a967eaebd8301d22798c73e2
Kiril <hello@nonamez.name>
2016-06-14 16:48:15 +0300

    Misc

--- resources/lang/lt/about.php
+++ resources/lang/lt/about.php
@@ -6,8 +6,8 @@ return [
 		'title' => 'Trumpai apie save:',
 		'li_1'  => 'Manau, kad visai neblogai žinau PHP, MySQL, JS/jQuery.',
 		'li_2'  => 'Esant reikalui galiu prisiminti HTML, CSS.',
-		'li_3'  => 'Kartais atrodo, kad žinau Python.',
-		'li_4'  => 'Esu skaitęs apie NodeJS.'
+		'li_3'  => 'Kartais atrodo, kad žinau NodeJS.',
+		'li_4'  => 'Esu skaitęs apie Python.'
 	],
 	'contact' => 'Jūs galite susisiekti su manimi el. paštu :img',
 	'ps' => 'Man patinka paprasti dalykai. Ir katės. Ir programavimas. Ir muzika. Ir sportas.'
