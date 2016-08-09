ef44f69c30dc9a30fcbb327891b86ac6147c06e1
Kiril <hello@nonamez.name>
2016-08-09 09:39:31 +0300

    Blog assets

---
+++ resources/assets/js/blog/post.js
@@ -0,0 +1,54 @@
+function getHeight(el) {
+	var s = el.style;
+	
+	var v   = s.visibility;
+	var p   = s.position;
+	var d   = s.display;
+	var m_h = s.maxHeight;
+	
+	s.visibility = 'hidden';
+	s.position   = 'absolute';
+	s.display    = 'block';
+	s.maxHeight  = 'none';
+	
+	var h = el.clientHeight;
+	
+	s.display    = d;
+	s.position   = p;
+	s.visibility = v;
+	s.maxHeight  = m_h;
+	
+	return h;
+}
+
+var articles = document.querySelectorAll('article');
+
+for (var i = 0, article; article = articles[i]; ++i) {
+	var post_content = article.querySelector('section.post-content');
+		post_content.setAttribute('data-real-heigt', getHeight(post_content) + 30); // Sometimes with a bigger text there comes some bug with height so we add some extra as a fix...
+
+	article.querySelector('header.post-header').addEventListener('click', function() {
+		var article = this.parentNode;
+		var post_content = article.querySelector('section.post-content');
+		
+		if (article.classList.contains('open')) {
+			article.classList.remove('open');
+			
+			post_content.style.maxHeight = 0;
+		} else {
+			var opened_articles = document.querySelectorAll('article.post.open');
+			
+			if (opened_articles.length > 0) {
+				for (var i = 0, o_article; o_article = opened_articles[i]; ++i) {
+					o_article.classList.remove('open');
+				
+					o_article.querySelector('section.post-content').style.maxHeight = 0;
+				}
+			}
+		
+			article.classList.add('open');
+			
+			post_content.style.maxHeight = post_content.getAttribute('data-real-heigt') + 'px';
+		}
+	});
+}
---
+++ resources/assets/js/blog/site.js
@@ -0,0 +1,104 @@
+function isDescendant(parent, child) {
+	var node = child.parentNode;
+
+	while (node != null) {
+		if (node == parent)
+			return true;
+
+		node = node.parentNode;
+	}
+
+	return false;
+}
+
+var dropdowns = document.querySelectorAll('[data-toggle=dropdown]');
+
+// Show menu
+for (var i = 0, dropdown; dropdown = dropdowns[i]; ++i) {
+	dropdown.addEventListener('click', function(event) {
+		for (var i = 0, dropdown; dropdown = dropdowns[i]; ++i)
+			dropdown.parentElement.classList.remove('open');
+		
+		this.parentElement.classList.toggle('open');
+		
+		return false;
+	});
+}
+
+// Hide menu
+document.addEventListener('click', function(event) {
+	var open_menu = document.querySelector('.menu.open');
+
+	if (open_menu != null) {
+		var target = event.target || event.srcElement;
+
+		if (isDescendant(open_menu, target) == false)
+			open_menu.classList.remove('open');
+	}
+});
+
+var top_element  = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', function() {
+	var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+	
+	if (scrolled > 100)
+		top_element.style.display = 'block';
+	else
+		top_element.style.display = 'none';
+})
+
+function getHeight(el) {
+	var s = el.style;
+	
+	var v   = s.visibility;
+	var p   = s.position;
+	var d   = s.display;
+	var m_h = s.maxHeight;
+	
+	s.visibility = 'hidden';
+	s.position   = 'absolute';
+	s.display    = 'block';
+	s.maxHeight  = 'none';
+	
+	var h = el.clientHeight;
+	
+	s.display    = d;
+	s.position   = p;
+	s.visibility = v;
+	s.maxHeight  = m_h;
+	
+	return h;
+}
+
+var articles = document.querySelectorAll('article');
+
+for (var i = 0, article; article = articles[i]; ++i) {
+	var post_content = article.querySelector('section.post-content');
+		post_content.setAttribute('data-real-heigt', getHeight(post_content) + 30); // Sometimes with a bigger text there comes some bug with height so we add some extra as a fix...
+
+	article.querySelector('header.post-header').addEventListener('click', function() {
+		var article = this.parentNode;
+		var post_content = article.querySelector('section.post-content');
+		
+		if (article.classList.contains('open')) {
+			article.classList.remove('open');
+			
+			post_content.style.maxHeight = 0;
+		} else {
+			var opened_articles = document.querySelectorAll('article.post.open');
+			
+			if (opened_articles.length > 0) {
+				for (var i = 0, o_article; o_article = opened_articles[i]; ++i) {
+					o_article.classList.remove('open');
+				
+					o_article.querySelector('section.post-content').style.maxHeight = 0;
+				}
+			}
+		
+			article.classList.add('open');
+			
+			post_content.style.maxHeight = post_content.getAttribute('data-real-heigt') + 'px';
+		}
+	});
+}
