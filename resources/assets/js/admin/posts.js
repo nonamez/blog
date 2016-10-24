ce3d860ab1fe78267ba5662596b0991391e7055f
Kiril <hello@nonamez.name>
2016-10-24 23:26:36 +0300

    Misc

--- resources/assets/js/admin/posts.js
+++ resources/assets/js/admin/posts.js
@@ -1,3 +1,18 @@
+const _ELEMENTS = {
+	input_tag_slug: jQuery('#tags-input-slug')
+}
+
 jQuery(document).ready(function() {
-	console.log('lol');
+	jQuery('#tags-button-create').click(function() {
+		var slug  = document.getElementById('tags-input-slug').value
+		var title = document.getElementById('tags-input-title').value
+
+		if (title.length) {
+			var container = jQuery('<span/>').addClass('label label-default tag').text(title).appendTo('#tags-div-container').after(' ')
+
+			jQuery('<span/>').attr('data-role', 'remove').appendTo(container).on('click', function() {
+				this.parentNode.parentNode.removeChild(this.parentNode)
+			})
+		}
+	})
 })
