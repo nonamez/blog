f26d3e48900a4cdf2c7c5b7461f8b5cd09d48cfb
Kiril Calkin <hello@nonamez.name>
2016-12-06 09:39:55 +0200

    Function movew to utils

--- app/Utils/File.php
+++ app/Utils/File.php
@@ -103,4 +103,32 @@ class File
 
 		return $new_file;
 	}
+
+	public static function getMimeType($file, $buffer = FALSE)
+	{
+		if (function_exists('finfo_file')) {
+			$finfo = new \finfo(FILEINFO_MIME);
+
+			if ($buffer) {
+				$mime = $finfo->buffer($file);
+			} else {
+				$mime = $finfo->file($file);
+			}
+
+			unset($finfo);
+
+			return $mime;
+		} else if (function_exists('mime_content_type')) {
+			return mime_content_type($file);
+		} else if (strpost(ini_get('disable_functions'), 'shell_exec') !== FALSE) {
+			$file = escapeshellarg($file);
+
+			$mime = shell_exec('file -bi ' . $file);
+			$mime = trim($mime);
+
+			return $mime;
+		} else {
+			return FALSE;
+		}
+	}
 }
