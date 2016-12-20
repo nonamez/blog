46b77a28616a6bda15658b9dc9ff4b9fa09bf904
Kiril <hello@nonamez.name>
2016-12-20 19:50:52 +0200

    Front-End moved to jQuery

--- resources/assets/js/blog.js
+++ resources/assets/js/blog.js
@@ -1,85 +1,54 @@
-var top_element  = document.getElementById('back-to-top')
+var posts       = jQuery('article:not(.post-standalone)')
+var html_body   = jQuery('html, body')
+var back_to_top = jQuery('#back-to-top')
 
-window.addEventListener('scroll', function() {
-	var scrolled = window.pageYOffset || document.documentElement.scrollTop
-	
-	if (scrolled > 100) {
-		top_element.style.display = 'block'
-	} else {
-		top_element.style.display = 'none'
-	}
-})
-
-function getHeight(el) {
-	var s = el.style
-	
-	var v   = s.visibility
-	var p   = s.position
-	var d   = s.display
-	var m_h = s.maxHeight
-	
-	s.visibility = 'hidden'
-	s.position   = 'absolute'
-	s.display    = 'block'
-	s.maxHeight  = 'none'
-	
-	var h = el.clientHeight
-	
-	s.display    = d
-	s.position   = p
-	s.visibility = v
-	s.maxHeight  = m_h
-	
-	return h
-}
-
-/*
-var articles = document.querySelectorAll('article:not(.post-standalone)')
-
-for (var i = 0, article; article = articles[i]; ++i) {
-	var post_content = article.querySelector('section.post-content')
-		post_content.setAttribute('data-real-heigt', getHeight(post_content) + 30) // Sometimes with a bigger text there comes some bug with height so we add some extra as a fix...
-
-	article.querySelector('header.post-header').addEventListener('click', function() {
-		var current_article = this.parentNode
-		var post_content = current_article.querySelector('section.post-content')
-
-		if (current_article.classList.contains('open')) {
-			current_article.classList.remove('open')
-
-			post_content.style.maxHeight = 0
-		} else {
-			var opened_articles = document.querySelectorAll('article.post.open')
-
-			if (opened_articles.length > 0) {
-				for (var i = 0, o_article; o_article = opened_articles[i]; ++i) {
-					o_article.classList.remove('open')
-
-					o_article.querySelector('section.post-content').style.maxHeight = 0
-				}
-			}
-
-			current_article.classList.add('open')
-
-			post_content.style.maxHeight = post_content.getAttribute('data-real-heigt') + 'px'
-		}
-	})
-}
-*/
 jQuery(document).ready(function() {
-	jQuery('article:not(.post-standalone)').each(function(key, post) {
-		var post = jQuery(this),
-			post_header  = post.find('header.post-header'),
-			post_content = post.find('section.post-content')
+	var posts = jQuery('article:not(.post-standalone)')
 
-		post_header.click(function() {
-			if (post.hasClass('open')) {
-				console.log('opened')
+	posts.each(function() {
+		var current_post = jQuery(this),
+		current_post_header  = current_post.find('header.post-header'),
+		current_post_content = current_post.find('section.post-content')
+
+		current_post_header.click(function() {
+			if (current_post.hasClass('open')) {
+				current_post_content.slideUp('slow', function() {
+					current_post.removeClass('open')
+				})
 			} else {
-				post_content.slideDown('slow', function() {
-					post.addClass('open')
+				posts.each(function() {
+					var post = jQuery(this)
+
+					if (post.hasClass('open')) {
+						post.find('section.post-content').slideUp('slow', function() {
+							post.removeClass('open')
+						})
+					}
+				})
+
+				current_post.addClass('open')
+				current_post_content.slideDown('slow', function() {
+					html_body.animate({
+						scrollTop: current_post.offset().top
+					}, 'slow')
 				})
 			}
 		})
+	})
+
+	back_to_top.click(function(event) {
+		event.preventDefault()
+
+		html_body.animate({scrollTop: 0}, 'slow')
+	})
+
+	html_body.on('scroll', function() {
+		var scrollTop = jQuery(window).scrollTop()
+
+		if (jQuery(window).scrollTop() > 100) {
+			back_to_top.show()
+		} else {
+			back_to_top.hide()
+		}
 	})
 })
