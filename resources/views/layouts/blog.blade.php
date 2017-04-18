588ee1e3275ba9339efc26c6c0a286657ead5c42
Kiril <hello@nonamez.name>
2017-04-18 19:33:12 +0300

    Misc

--- resources/views/layouts/blog.blade.php
+++ resources/views/layouts/blog.blade.php
@@ -81,7 +81,7 @@
 						<section class="poweredby">
 							<a href="http://laravel.com" rel="nofollow">Laravel</a> |
 							<a href="https://github.com/suinly/science" rel="nofollow">Science</a> |
-							<a href="https://www.digitalocean.com/?refcode=0bbcba259691" rel="nofollow">DigitalOcean</a>
+							<a href="https://m.do.co/c/0bbcba259691" rel="nofollow">DigitalOcean</a>
 						</section>
 					</div>
 				</footer>
