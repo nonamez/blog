8f5faf48759d1a1a19008303ba852162b0c72bb4
Kirill <hello@nonamez.name>
2017-09-12 16:54:01 +0300

    Misc

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,9 +23,6 @@ const { mix } = require('laravel-mix');
 
 mix.sass('resources/assets/sass/admin.scss', 'public/css/admin.css').version()
 mix.sass('resources/assets/sass/blog/style.scss', 'public/css/blog.css').version()
-mix.sass('resources/assets/sass/portfolio.scss', 'public/css/portfolio.css').version()
 
 mix.js('resources/assets/js/blog.js', 'public/js/blog.js').version()
 mix.js('resources/assets/js/admin/admin.js', 'public/js/admin.js').version()
-mix.copy('resources/assets/js/admin/old_components/portfolio/works.js', 'public/js/admin/portfolio/works.js').version()
-
