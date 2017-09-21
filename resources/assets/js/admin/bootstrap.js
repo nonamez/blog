f0cb16ae7da601e06379ddf7f035fdd49b6810f9
Kirill <hello@nonamez.name>
2017-09-21 09:10:01 +0300

    toastr

--- resources/assets/js/admin/bootstrap.js
+++ resources/assets/js/admin/bootstrap.js
@@ -1,12 +1,52 @@
 window.$ = window.jQuery = require('jquery')
 
+window.toastr = require('toastr')
+
 jQuery.ajaxSetup({
 	cache: false,
 	dataType: 'JSON',
 	headers: {'X-CSRF-Token': jQuery('meta[name="csrf-token"]').attr('content')}
 })
 
-window.toastr = require('toastr')
+jQuery(document).ajaxStart(function() {
+	jQuery('.has-error').removeClass('has-error').find('.help-block').text('').addClass('hide')
+})
+
+jQuery(document).ajaxError(function(event, jqXHR, ajaxSettings, errorThrown) {
+	if (jqXHR.status == 422) {
+		jQuery.each(jqXHR.responseJSON, function(key, text) {
+			let element = jQuery('[name="' + key + '"]')
+
+				// In case of array
+				if (element.length == 0) {
+					element = jQuery('[name="' + key + '[]"]')
+				}
+
+				if (element.length == 0) {
+					toastr.error(text)
+					return
+				}
+
+				element.closest('.form-group').addClass('has-error')
+
+				let help_block = element.parent().find('.help-block')
+
+				if (help_block.length > 0) {
+					help_block.text(text).removeClass('hide')
+				} else {
+					toastr.warning(text)
+				}
+			})
+	} else if (jqXHR.status == 404) {
+		toastr.warning('Item not found')
+	} else {
+		if (jqXHR.hasOwnProperty('responseJSON')) {
+			toastr.error(jqXHR.responseJSON.message)
+		} else {
+			toastr.error('A critical error has occured. Please reload the page and try again')
+		}
+	}
+})
 
 window.Vue = require('vue')
 
