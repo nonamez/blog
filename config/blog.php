f2789c7fc29881d519d6e14fd2ceacb87adb4708
Kirill <hello@nonamez.name>
2018-01-10 11:46:44 +0200

    Pagination

--- config/blog.php
+++ config/blog.php
@@ -3,7 +3,6 @@
 return array(
 	'allowed_files'    => ['jpeg', 'bmp', 'png', 'zip', 'pdf'],
 	'tags_in_header'   => 10,
-	'posts_per_page'   => 10,
 	'google_analytics' => env('GOOGLE_ANALYTICS', FALSE),
 	'disqus_shortname' => env('DISQUS_SHORTNAME', FALSE),
 	'disqus_publickey' => env('DISQUS_PUBLICKEY', FALSE)
