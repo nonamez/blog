3c83d9d1b11bdd122454ca7436588dc6fa733c4d
Kiril <hello@nonamez.name>
2018-01-13 11:03:38 +0200

    misc

--- resources/views/blog/posts.blade.php
+++ resources/views/blog/posts.blade.php
@@ -2,18 +2,19 @@
 @section('content')
 <main class="content" role="main">
 	@forelse ($posts as $post)
-	<a href="{{ $post->getURL() }}">
-		<article class="post{{ $post->getPostClass() }}" id="post-{{ $post->id }}">
-			<header class="post-header">
-				<div class="icon">
-					<i class="icon-align-left"></i>
-				</div>
-				<div class="post-title">{{ $post->title }}</div>
-				<div class="post-meta">
-					<time datetime="{{ $post->date }}">{{ $post->date->diffForHumans() }}</time>
-				</div>
-			</header>
-		</article>
+		<a href="{{ $post->getURL() }}">
+			<article class="post{{ $post->getPostClass() }}" id="post-{{ $post->id }}">
+				<header class="post-header">
+					<div class="icon">
+						<i class="icon-align-left"></i>
+					</div>
+					<div class="post-title">{{ $post->title }}</div>
+					<div class="post-meta">
+						<time datetime="{{ $post->date }}">{{ $post->date->diffForHumans() }}</time>
+					</div>
+				</header>
+			</article>
+		</a>
 		{{-- <section class="post-content">
 			<ul class="post-tags list-inline">
 				@include('blog.partials.line')
@@ -25,7 +26,6 @@
 				<a href="{{ $post->routes['preview'] }}" class="btn btn-default btn-sm">@lang('blog.post.more_link')</a>
 			</p>
 		</section> --}}
-	</a>
 	@empty
 	<div id="no-posts">@lang('blog.post.no_posts')</div>
 	@endforelse
