dcf42f2746ead758270bb2880cca493e379c6e08
Kiril <hello@nonamez.name>
2020-01-25 19:03:24 +0200

    misc

--- database/migrations/2019_11_28_142212_rename_parent_post_id_to_parent_id_column_in_blg_translated_posts_table.php
+++ database/migrations/2019_11_28_142212_rename_parent_post_id_to_parent_id_column_in_blg_translated_posts_table.php
@@ -13,10 +13,7 @@ class RenameParentPostIdToParentIdColumnInBlgTranslatedPostsTable extends Migrat
 	 */
 	public function up()
 	{
-		Schema::table('blg_translated_posts', function (Blueprint $table) {
-			DB::statement('ALTER TABLE `nnm_blg_translated_posts` CHANGE `parent_post_id` `parent_id` INT(10)  UNSIGNED  NOT NULL;');
-
-		});
+		DB::statement('ALTER TABLE `nnm_blg_translated_posts` CHANGE `parent_post_id` `parent_id` INT(10)  UNSIGNED  NOT NULL;');
 	}
 
 	/**
@@ -26,8 +23,6 @@ class RenameParentPostIdToParentIdColumnInBlgTranslatedPostsTable extends Migrat
 	 */
 	public function down()
 	{
-		Schema::table('blg_translated_posts', function (Blueprint $table) {
-			DB::statement('ALTER TABLE `nnm_blg_translated_posts` CHANGE `parent_id` `parent_post_id` INT(10)  UNSIGNED  NOT NULL;');
-		});
+		DB::statement('ALTER TABLE `nnm_blg_translated_posts` CHANGE `parent_id` `parent_post_id` INT(10)  UNSIGNED  NOT NULL;');
 	}
 }
--- database/migrations/2019_12_08_131223_alter_translated_posts_change_meta.php
+++ database/migrations/2019_12_08_131223_alter_translated_posts_change_meta.php
@@ -13,12 +13,10 @@ class AlterTranslatedPostsChangeMeta extends Migration
      */
     public function up()
     {
-        /* 13:13:00 localhost nonamez */ ALTER TABLE `nnm_blg_translated_posts` CHANGE `meta_keywords` `meta_keywords` VARCHAR(255)  CHARACTER SET utf8  COLLATE utf8_unicode_ci  NULL  DEFAULT '';
-        /* 13:12:51 localhost nonamez */ ALTER TABLE `nnm_blg_translated_posts` CHANGE `meta_description` `meta_description` VARCHAR(255)  CHARACTER SET utf8  COLLATE utf8_unicode_ci  NULL  DEFAULT '';
-        /* 13:12:48 localhost nonamez */ ALTER TABLE `nnm_blg_translated_posts` DROP `meta_title`;
 
-
-
+        DB::statement('ALTER TABLE `nnm_blg_translated_posts` CHANGE `meta_keywords` `meta_keywords` VARCHAR(255)  CHARACTER SET utf8  COLLATE utf8_unicode_ci  NULL  DEFAULT '';');
+        DB::statement('ALTER TABLE `nnm_blg_translated_posts` CHANGE `meta_description` `meta_description` VARCHAR(255)  CHARACTER SET utf8  COLLATE utf8_unicode_ci  NULL  DEFAULT '';');
+        DB::statement('ALTER TABLE `nnm_blg_translated_posts` DROP `meta_title`;');
     }
 
     /**
