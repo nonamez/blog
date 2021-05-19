151e7a78b2255fdcd761a3e9288c6dee05fc0ca1
Kiril <hello@nonamez.name>
2021-05-19 08:40:09 +0300

    langs

---
+++ resources/lang/lt/blog.php
@@ -0,0 +1,35 @@
+<?php
+
+return [
+	'title'  => 'Kiril',
+	'meta' => [
+		'description' => 'Tai yra savamokslio web programuotojo iš Klaipėdos asmeninė svetainė',
+		'keywords' => 'Svetainių kūrimas, Programavimas, Laravel, PHP, MySQL, Pyhton, JavaScript, NodeJS, Socket.IO, Ionic, Mongoose, SlimPHP',
+	],
+	'header' => [
+		'menu' => [
+			'home'    => 'Pagrintinis',
+			'about'   => 'Apie mane',
+			'contact' => 'Кontaktai'
+		],
+		'title'       => 'Sveiki! Aš Kirilas.',
+		'description' => 'Mintys, istorijos ir idėjos apie programavimą.'
+	],
+	'post' => [
+		'no_posts'      => 'Įrašų nerasta',
+		'more_link'     => 'Skaityti toliau',
+		'comments_link' => 'Komentarai',
+		'no_comments'   => 'Komentarų nera',
+		'show_comments' => 'Rodyti komentarus'
+	],
+	'footer' => [
+		'proudly_made_with'  => 'Sukurta su',
+		'design_inspired_by' => 'Dizainas įkvėptas'
+	],
+	'about' => [
+		'iam' => 'Sveiki! Mano vardas Kirilas. Aš esu savamokslis web programuotojas iš Klaipėdos.',
+		'contact' => 'Jūs galite susisiekti su manimi el. paštu :img arba :telegram',
+		'ps' => 'Man patinka paprasti dalykai. Ir katės. Ir dviračiai. Ir bėgimas. Ir plaukimas'
+	]
+];
+?>
